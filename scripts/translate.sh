87a190bdd14bdbf6da072666f232875b3c2f7aa0
Ernad Husremovic <hernad@bring.out.ba>
2020-10-09 18:14:17 +0200

    update translations

--- scripts/translate.sh
+++ scripts/translate.sh
@@ -8,8 +8,10 @@ do
     TRANS=`echo $TRANS | sed -e 's/\[comma\]/,/g'`
     ORIG=`echo $ORIG | sed -e 's/\[space\]/ /g'`
     TRANS=`echo $TRANS | sed -e 's/\[space\]/ /g'`
+    # x5c - backslash, x27 - quote
+    TRANS=`echo $TRANS | sed -e "s@\[quote\]@\\\\\x5c\\\\\x27@g"`
 
    
-    echo "$ORIG -> $TRANS"
+    echo "'$ORIG' -> '$TRANS'"
     find -name  '*.lsf' | xargs sed -i "s@'$ORIG'@'$TRANS'@g"
 done < translate.csv
