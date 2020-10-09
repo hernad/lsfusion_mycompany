d03917fcc852074b51a2da2db3422c7302f31e48
Ernad Husremovic <hernad@bring.out.ba>
2020-10-09 16:05:00 +0200

    update translate.csv

--- scripts/translate.sh
+++ scripts/translate.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+
 while IFS=, read -r ORIG TRANS
 do
+    
+    ORIG=`echo $ORIG | sed -e 's/\[comma\]/,/g'`
+    TRANS=`echo $TRANS | sed -e 's/\[comma\]/,/g'`
+    ORIG=`echo $ORIG | sed -e 's/\[space\]/ /g'`
+    TRANS=`echo $TRANS | sed -e 's/\[space\]/ /g'`
+
+   
     echo "$ORIG -> $TRANS"
     find -name  '*.lsf' | xargs sed -i "s@'$ORIG'@'$TRANS'@g"
 done < translate.csv
